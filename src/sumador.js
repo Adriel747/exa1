d7233704570a4161dd02aa33fb9f317ac2eb91c0
Adriel747 <adriel.fernandez@ucb.edu.bo>
2023-09-05 10:34:27 -0400

    se intento hacer la tercera prueba esta en comentarios pero se realizo 2 pruebas mas al test de los 2 commits anteriores

--- src/sumador.js
+++ src/sumador.js
@@ -5,6 +5,14 @@ function conteo(str){
   let conteo = str.length;
   return conteo;
 }
+/*
+function conteoPalabra(str){
+  let conteo = str.length;
+  let i=0;
+  
+  for (i; i<= conteo;i++){
 
-
+  }
+}
+*/
 export {parrafo,conteo};
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -4,7 +4,13 @@ describe("parrafo", () => {
   it("deberia devolver la cadena", () => {
     expect(parrafo("hola como estan")).toEqual("hola como estan");
   });
+  it("deberia devolver la cadena", () => {
+    expect(parrafo("hola miami como esta")).toEqual("hola miami como esta");
+  });
   it("deberia devolver numero de la cadena", () => {
     expect(conteo("hola mundo")).toEqual(10);
+  });
+  it("deberia devolver numero de la cadena", () => {
+    expect(conteo("hola gente de bolivia")).toEqual(21);
   });
 });
